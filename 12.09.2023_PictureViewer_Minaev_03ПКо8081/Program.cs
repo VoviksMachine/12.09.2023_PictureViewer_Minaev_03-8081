2c24f400a4166cea18a5cc81011238e894d4d8f9
VoviksMachine <VoviksMachine@VOVIKS>
2023-09-12 18:35:52 +0300

    Добавьте файлы проекта.

---
+++ 12.09.2023_PictureViewer_Minaev_03ПКо8081/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12._09._2023_PictureViewer_Minaev_03ПКо8081
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
